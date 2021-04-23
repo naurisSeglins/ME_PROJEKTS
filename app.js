6c390d5b13b420bc18631163202cd192d87e3319
naurisSeglins <75720697+naurisSeglins@users.noreply.github.com>
2021-04-23 21:43:23 +0300

    uz 2. nodošanu
    2.nodošana trūkst tikai responsive, un teksti kā arī projekta lapa

--- app.js
+++ app.js
@@ -68,3 +68,19 @@ services.addEventListener("click", function (e) {
     element.classList.add("active");
   }
 });
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: 56.95066458737458, lng: 24.10616708483712 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 14,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
